cee500f90fd5ff1f0dc1dcb5b1f51b2c3b323a70
Ayu-KW <ayukawabata1109@gmail.com>
2023-06-01 23:54:29 +0900

    ヘッダー・フッダーのざっくりしたスタイル反映（修正）

---
+++ .history/src/components/layout/Header_20230601234729.tsx
@@ -0,0 +1,12 @@
+export const Header = () => {
+  return (
+    <header className="bg-sky-600 p-4 text-white flex justify-around">
+      <h1 className="text-xl font-bold">ヒアリングシートアプリ</h1>
+      <nav className="flex gap-4">
+        <p>新規登録</p>
+        <p>顧客一覧</p>
+        <button className="border-white">ログアウト</button>
+      </nav>
+    </header>
+  );
+};
--- .history/src/components/layout/Header_20230601235336.tsx
+++ .history/src/components/layout/Header_20230601235336.tsx
@@ -1,7 +1,7 @@
 export const Header = () => {
   return (
     <header className="bg-sky-600 p-4 text-white flex justify-around">
-      <h1 className="text-2xl font-bold">ヒアリングシートアプリ</h1>
+      <h1 className="text font-bold">ヒアリングシートアプリ</h1>
       <nav className="flex gap-6 items-center font-bold">
         <p>新規登録</p>
         <p>顧客一覧</p>
